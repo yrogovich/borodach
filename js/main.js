3386e183f41c9a3475a473136cabc3242f20a0b0
Yaroslav <yRogovich@gmail.com>
2019-09-20 10:23:49 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(function() {
+    SmoothScroll({ 
+        animationTime: 800,
+        stepSize: 85 
+    });
+    
+    // Lazyloading
+    $('.lazy').Lazy({
+        // your configuration goes here
+        scrollDirection: 'vertical',
+        effect: 'fadeIn',
+        effectTime: 500,
+        //threshold: 0,
+        visibleOnly: true,
+        onError: function(element) {
+            console.log('error loading ' + element.data('src'));
+        }
+    });
+    // // Mobile burger script
+    // $('.burger').click(function() {
+    //     $(this).toggleClass('active');
+    //     if($('.primary-menu-container').css('display') === 'flex') {
+    //         $('.primary-menu-container')
+    //             .fadeOut(300, function() {
+    //                 if($(this).css('display') === 'none')
+    //                 $(this).removeAttr('style');
+    //         });
+    //     } else {               
+    //         $('.primary-menu-container')
+    //             .css("display", "flex")
+    //             .hide()
+    //             .fadeIn(300, function() {
+    //                 if($(this).css('display') === 'none')
+    //                 $(this).removeAttr('style');
+    //         });
+    //     }
+    // });
+    
+
+    // // Masks for input
+    // $('.phone-mask').mask('+375 (00) 000-00-00');
+    // $('.date-mask').mask('00/00');
+    // $('.time-mask').mask('00:00');
+});

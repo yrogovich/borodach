16cbaef04cf74c4a9f40da469a39d68845c795d9
Yaroslav <yRogovich@gmail.com>
2019-09-24 11:44:47 +0300

    Git release v1

--- js/main.js
+++ js/main.js
@@ -15,9 +15,22 @@ $(function() {
         infinite: true,
         slidesToShow: 1,
         fade: true,
+        lazyLoad: 'ondemand',
         prevArrow: '<img class="arrow arrow-left" src="img/reviews/arrow-left-circle.svg" alt="arrow-left">',
         nextArrow: '<img class="arrow arrow-right" src="img/reviews/arrow-right-circle.svg" alt="arrow-right">',
     });
+
+    //  Show ymaps on scroll
+    var point = $('#boroda-features');
+    var pointTop = point.offset().top;
+    var handler = function () {
+        var windowTop = $(this).scrollTop();
+        if (windowTop > pointTop) {
+            $('#map').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Af9e260f4a9bda6fa194559dc9b3dbf231a510e156af062e8a4e5d3de1e3a1552&amp;width=100%25&amp;height=100%&amp;lang=ru_RU&amp;scroll=false"></script>"></script>');
+            $(window).unbind( "scroll", handler );
+        }
+    };
+    $(window).bind( "scroll", handler );  
 
     var images = document.querySelectorAll('.parrallax-speed-3 img');
     new simpleParallax(images, {
@@ -49,18 +62,13 @@ $(function() {
 	    transition: 'cubic-bezier(0,0,0,1)'
     });
 
-    // var images = document.querySelectorAll('.parrallax-speed-2 img');
-    // new simpleParallax(images, {
-    //     delay: 0,
-    //     orientation: 'down',
-    //     scale: 1.5,
-    //     overfow: true,
-    //     delay: .6,
-	//     transition: 'cubic-bezier(0,0,0,1)'
-    // });
-
     $('.burger').click(function() {
         $('.mobile-menu').toggleClass('active');
+    });
+
+    $('.mobile-menu a').click(function() {
+        $('.burger').removeClass('active');
+        $('.mobile-menu').removeClass('active');
     });
     
     // Lazyloading
